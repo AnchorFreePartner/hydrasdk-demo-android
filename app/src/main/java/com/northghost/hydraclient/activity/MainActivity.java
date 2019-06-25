7b7f9db1ba17ca184518657d62e2ef5cca12d0c2
Timoshenko Alexandr <thick.tav@gmail.com>
2019-06-25 18:28:15 +0200

    list sdk methods

--- app/src/main/java/com/northghost/hydraclient/activity/MainActivity.java
+++ app/src/main/java/com/northghost/hydraclient/activity/MainActivity.java
@@ -1,11 +1,15 @@
 package com.northghost.hydraclient.activity;
 
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import com.anchorfree.hydrasdk.HydraSDKConfig;
 import com.anchorfree.hydrasdk.HydraSdk;
 import com.anchorfree.hydrasdk.SessionConfig;
 import com.anchorfree.hydrasdk.SessionInfo;
 import com.anchorfree.hydrasdk.api.AuthMethod;
+import com.anchorfree.hydrasdk.api.ClientInfo;
 import com.anchorfree.hydrasdk.api.data.Country;
 import com.anchorfree.hydrasdk.api.data.ServerCredentials;
 import com.anchorfree.hydrasdk.api.response.RemainingTraffic;
@@ -13,6 +17,7 @@ import com.anchorfree.hydrasdk.api.response.User;
 import com.anchorfree.hydrasdk.callbacks.Callback;
 import com.anchorfree.hydrasdk.callbacks.CompletableCallback;
 import com.anchorfree.hydrasdk.callbacks.TrafficListener;
+import com.anchorfree.hydrasdk.callbacks.VpnCallback;
 import com.anchorfree.hydrasdk.callbacks.VpnStateListener;
 import com.anchorfree.hydrasdk.compat.CredentialsCompat;
 import com.anchorfree.hydrasdk.dns.DnsRule;
@@ -21,11 +26,18 @@ import com.anchorfree.hydrasdk.exceptions.HydraException;
 import com.anchorfree.hydrasdk.exceptions.NetworkRelatedException;
 import com.anchorfree.hydrasdk.exceptions.RequestException;
 import com.anchorfree.hydrasdk.exceptions.VPNException;
+import com.anchorfree.hydrasdk.fireshield.FireshieldCategory;
+import com.anchorfree.hydrasdk.fireshield.FireshieldConfig;
+import com.anchorfree.hydrasdk.vpnservice.ConnectionStatus;
 import com.anchorfree.hydrasdk.vpnservice.VPNState;
+import com.anchorfree.hydrasdk.vpnservice.config.VpnConfig;
+import com.anchorfree.hydrasdk.vpnservice.connectivity.NotificationConfig;
+import com.anchorfree.hydrasdk.vpnservice.credentials.AppPolicy;
 import com.anchorfree.reporting.TrackingConstants;
 import com.northghost.hydraclient.MainApplication;
 import com.northghost.hydraclient.dialog.LoginDialog;
 import com.northghost.hydraclient.dialog.RegionChooserDialog;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -60,7 +72,7 @@ public class MainActivity extends UIActivity implements TrafficListener, VpnStat
     }
 
     @Override
-    public void vpnError(HydraException e) {
+    public void vpnError(VPNException e) {
         updateUI();
         handleError(e);
     }
@@ -95,7 +107,7 @@ public class MainActivity extends UIActivity implements TrafficListener, VpnStat
     protected void logOutFromVnp() {
         showLoginProgress();
 
-        HydraSdk.logout(CompletableCallback.EMPTY);
+        HydraSdk.logout();
         selectedCountry = "";
 
         hideLoginProgress();
@@ -194,7 +206,6 @@ public class MainActivity extends UIActivity implements TrafficListener, VpnStat
                             callback.success(selectedCountry);
                         }
                     });
-
                 } else {
                     callback.success(selectedCountry);
                 }
@@ -304,5 +315,377 @@ public class MainActivity extends UIActivity implements TrafficListener, VpnStat
                     break;
             }
         }
+    }
+
+    //fake method to support migration documentation and list all available methods
+    public void sdkMethodsList() {
+        HydraSdk.deletePurchase(0, CompletableCallback.EMPTY);
+        HydraSdk.getStartVpnTimestamp(new Callback<Long>() {
+            @Override
+            public void success(@NonNull Long aLong) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.getVpnState(new Callback<VPNState>() {
+            @Override
+            public void success(@NonNull VPNState vpnState) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.getConnectionStatus(new Callback<ConnectionStatus>() {
+            @Override
+            public void success(@NonNull ConnectionStatus connectionStatus) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.remainingTraffic(new Callback<RemainingTraffic>() {
+            @Override
+            public void success(@NonNull RemainingTraffic remainingTraffic) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.restartVpn("", TrackingConstants.GprReasons.A_APP_RUN, AppPolicy.newBuilder().build(), new Bundle(), new Callback<Bundle>() {
+            @Override
+            public void success(@NonNull Bundle bundle) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.setLoggingLevel(Log.VERBOSE);
+        HydraSdk.isPausedForReconnection();
+        HydraSdk.collectDebugInfo(new Callback<HydraSdk.DebugInfo>() {
+            @Override
+            public void success(@NonNull HydraSdk.DebugInfo debugInfo) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.collectDebugInfo(new Callback<HydraSdk.DebugInfo>() {
+            @Override
+            public void success(@NonNull HydraSdk.DebugInfo debugInfo) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.purchase("", new CompletableCallback() {
+            @Override
+            public void complete() {
+
+            }
+
+            @Override
+            public void error(HydraException e) {
+
+            }
+        });
+        HydraSdk.purchase("", "", new CompletableCallback() {
+            @Override
+            public void complete() {
+
+            }
+
+            @Override
+            public void error(HydraException e) {
+
+            }
+        });
+        HydraSdk.getConnectionStatus(new Callback<ConnectionStatus>() {
+            @Override
+            public void success(@NonNull ConnectionStatus connectionStatus) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.current(new Callback<User>() {
+            @Override
+            public void success(@NonNull User user) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.getDeviceId();
+        HydraSdk.currentUser(new Callback<User>() {
+            @Override
+            public void success(@NonNull User user) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.getSessionInfo(new Callback<SessionInfo>() {
+            @Override
+            public void success(@NonNull SessionInfo sessionInfo) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.getAccessToken();
+        HydraSdk.getTrafficStats();
+        HydraSdk.requestVpnPermission(new CompletableCallback() {
+            @Override
+            public void complete() {
+
+            }
+
+            @Override
+            public void error(HydraException e) {
+
+            }
+        });
+        HydraSdk.getConnectionAttemptId();
+        HydraSdk.getStartVpnTimestamp();
+        HydraSdk.getVpnState();
+        HydraSdk.getTrafficStats();
+        HydraSdk.getLoggingLevel();
+        HydraSdk.isABISupported();
+        HydraSdk.isLoggingEnabled();
+        HydraSdk.isVpnStarted();
+        HydraSdk.isLoggingEnabled();
+        HydraSdk.collectDebugInfo(new Callback<HydraSdk.DebugInfo>() {
+            @Override
+            public void success(@NonNull HydraSdk.DebugInfo debugInfo) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.collectDebugInfo(this, new Callback<String>() {
+            @Override
+            public void success(@NonNull String s) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.current(new Callback<User>() {
+            @Override
+            public void success(@NonNull User user) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.startVPN("", new Callback<ServerCredentials>() {
+            @Override
+            public void success(@NonNull ServerCredentials serverCredentials) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.startVPN("", "", new Callback<ServerCredentials>() {
+            @Override
+            public void success(@NonNull ServerCredentials serverCredentials) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.startVPNExceptApps(new ArrayList<>(), "", new Callback<ServerCredentials>() {
+            @Override
+            public void success(@NonNull ServerCredentials serverCredentials) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.startVPNExceptApps("", new ArrayList<>(), "", new Callback<ServerCredentials>() {
+            @Override
+            public void success(@NonNull ServerCredentials serverCredentials) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.startVPNForApps(new ArrayList<>(), "", new Callback<ServerCredentials>() {
+            @Override
+            public void success(@NonNull ServerCredentials serverCredentials) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.startVPNForApps("", new ArrayList<>(), "", new Callback<ServerCredentials>() {
+            @Override
+            public void success(@NonNull ServerCredentials serverCredentials) {
+
+            }
+
+            @Override
+            public void failure(@NonNull HydraException e) {
+
+            }
+        });
+        HydraSdk.addTrafficListener(new TrafficListener() {
+            @Override
+            public void onTrafficUpdate(long l, long l1) {
+
+            }
+        });
+        HydraSdk.addVpnCallListener(new VpnCallback() {
+            @Override
+            public void onVpnCall(Parcelable parcelable) {
+
+            }
+        });
+        HydraSdk.addVpnListener(new VpnStateListener() {
+            @Override
+            public void vpnStateChanged(VPNState vpnState) {
+
+            }
+
+            @Override
+            public void vpnError(VPNException e) {
+
+            }
+        });
+        HydraSdk.removeTrafficListener(null);
+        HydraSdk.removeVpnCallListener(null);
+        HydraSdk.removeVpnListener(null);
+
+        HydraSdk.init(this, ClientInfo.newBuilder()
+                        .baseUrl("")
+                        .carrierId("")
+                        .email("")
+                        .build(),
+                NotificationConfig.newBuilder()
+                        .inConnected("", "")
+                        .channelId("")
+                        .clickAction("")
+                        .disabled()
+                        .enableConnectionLost()
+                        .icon(null)
+                        .inConnecting("", "")
+                        .inIdle("", "")
+                        .title("")
+                        .smallIconId(0)
+                        .build(),
+                HydraSDKConfig.newBuilder()
+                        .captivePortal(false)
+                        .moveToIdleOnPause(false)
+                        .observeNetworkChanges(true)
+                        .unsafeClient(false)
+                        .addBlacklistDomain("")
+                        .addBlacklistDomains(0)
+                        .addBypassDomain("")
+                        .addBypassDomains(0)
+                        .addBypassDomain(VpnConfig.MODE.BYPASS, "")
+                        .addBypassDomain(VpnConfig.MODE.PROXY_PEER, "")
+                        .addBypassDomain(VpnConfig.MODE.VPN, "")
+                        .build());
+        HydraSdk.update(NotificationConfig.newBuilder()
+                .inConnected("", "")
+                .channelId("")
+                .clickAction("")
+                .disabled()
+                .enableConnectionLost()
+                .icon(null)
+                .inConnecting("", "")
+                .inIdle("", "")
+                .title("")
+                .smallIconId(0)
+                .build());
+        HydraSdk.updateConfig(new SessionConfig.Builder()
+                .addDnsRule(DnsRule.Builder.block().fromAssets(""))
+                .addDnsRule(DnsRule.Builder.bypass().fromAssets(""))
+                .addDnsRule(DnsRule.Builder.proxy().fromAssets(""))
+                .addDnsRule(DnsRule.Builder.vpn().fromAssets(""))
+                .addDnsRule(DnsRule.Builder.vpn().fromDomains(new ArrayList<>()))
+                .addDnsRule(DnsRule.Builder.vpn().fromFile(""))
+                .addDnsRule(DnsRule.Builder.vpn().fromResource(0))
+                .exceptApps(new ArrayList<>())
+                .forApps(new ArrayList<>())
+                .withVirtualLocation("")
+                .withPolicy(AppPolicy.newBuilder().build())
+                .withFireshieldConfig(new FireshieldConfig.Builder()
+                        .addCategory(FireshieldCategory.Builder.block(""))
+                        .addCategory(FireshieldCategory.Builder.blockAlertPage(""))
+                        .addCategory(FireshieldCategory.Builder.bypass(""))
+                        .addCategory(FireshieldCategory.Builder.custom("", ""))
+                        .addCategory(FireshieldCategory.Builder.proxy(""))
+                        .addCategory(FireshieldCategory.Builder.vpn(""))
+                        .build())
+                .build(), new CompletableCallback() {
+            @Override
+            public void complete() {
+
+            }
+
+            @Override
+            public void error(HydraException e) {
+
+            }
+        });
     }
 }
