c88fffb55da1321f50b413cb5e3be623dd16f948
nikslushkin <n.slushkin@anchorfree.com>
2020-10-09 14:46:56 -0700

    some refactoring

--- libs/hydrasdk-rx/src/main/java/com/anchorfree/hydrasdk/rx/UnifiedSdkExtensions.kt
+++ libs/hydrasdk-rx/src/main/java/com/anchorfree/hydrasdk/rx/UnifiedSdkExtensions.kt
@@ -7,35 +7,27 @@ import com.anchorfree.vpnsdk.vpnservice.VPNState
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 
-interface UnifiedSdkRx {
-    fun observeVpnState(): Observable<VPNState>
-
-    fun <T : Parcelable> observeVpnCalls(): Observable<T>
+fun UnifiedSDK.observeVpnState(): Observable<VPNState> = Observable.create {
+    val listener = RxVpnStateListener(it)
+    UnifiedSDK.addVpnStateListener(listener)
+    it.setCancellable { UnifiedSDK.removeVpnStateListener(listener) }
 }
 
-class UnifiedSdkRxImpl {
-    fun observeVpnState(): Observable<VPNState> = Observable.create {
-        val listener = RxVpnStateListener(it)
-        UnifiedSDK.addVpnStateListener(listener)
-        it.setCancellable { UnifiedSDK.removeVpnStateListener(listener) }
-    }
+fun <T : Parcelable> UnifiedSDK.observeVpnCalls(): Observable<T> = Observable.create {
+    val listener = RxVpnCallback<T>(it)
+    UnifiedSDK.addVpnCallListener(listener)
+    it.setCancellable { UnifiedSDK.removeVpnCallListener(listener) }
+}
 
-    fun <T : Parcelable> observeVpnCalls(): Observable<T> = Observable.create {
-        val listener = RxVpnCallback<T>(it)
-        UnifiedSDK.addVpnCallListener(listener)
-        it.setCancellable { UnifiedSDK.removeVpnCallListener(listener) }
-    }
+fun UnifiedSDK.getStatus(): Single<SessionInfo> = Single.create {
+    val listener = RxSingleCallback<SessionInfo>(it)
+    UnifiedSDK.getStatus(listener)
+}
 
-    fun getStatus(): Single<SessionInfo> = Single.create {
-        val listener = RxSingleCallback<SessionInfo>(it)
-        UnifiedSDK.getStatus(listener)
-    }
-
-    fun observeTraffic(): Observable<Traffic> = Observable.create {
-        val listener = RxTrafficCallback(it)
-        UnifiedSDK.addTrafficListener(listener)
-        it.setCancellable {
-            UnifiedSDK.removeTrafficListener(listener)
-        }
+fun UnifiedSDK.observeTraffic(): Observable<Traffic> = Observable.create {
+    val listener = RxTrafficCallback(it)
+    UnifiedSDK.addTrafficListener(listener)
+    it.setCancellable {
+        UnifiedSDK.removeTrafficListener(listener)
     }
 }
