95d27b1226bb96d0cd24ae5ba2b83cf8b68c2932
Alexandr Timoshenko <alexandr.timoshenko@pango.co>
2024-09-19 08:56:07 +0200

    Add sample of vpn#restart usage to sdk demo app

--- app/src/main/java/com/northghost/hydraclient/activity/MainActivity.java
+++ app/src/main/java/com/northghost/hydraclient/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import unified.vpn.sdk.*;
+
 import com.northghost.hydraclient.MainApplication;
 import com.northghost.hydraclient.adapter.RegionListAdapter;
 import com.northghost.hydraclient.dialog.LoginDialog;
@@ -16,7 +17,7 @@ import java.util.List;
 
 public class MainActivity extends UIActivity implements TrafficListener, VpnStateListener,
         LoginDialog.LoginConfirmationInterface, RegionChooserDialog.RegionChooserInterface {
-
+    public static final String TAG = "MainActivity";
     private String selectedCountry = "";
 
     @Override
@@ -112,45 +113,23 @@ public class MainActivity extends UIActivity implements TrafficListener, VpnStat
     }
 
     @Override
-    protected void connectToVpn() {
+    protected void connectToVpn(CompletableCallback callback) {
         isLoggedIn(new Callback<Boolean>() {
             @Override
             public void success(@NonNull Boolean aBoolean) {
                 if (aBoolean) {
-                    List<String> fallbackOrder = new ArrayList<>();
-                    fallbackOrder.add(HydraTransport.TRANSPORT_ID);
-                    fallbackOrder.add(OpenVpnTransport.TRANSPORT_ID_TCP);
-                    fallbackOrder.add(OpenVpnTransport.TRANSPORT_ID_UDP);
                     showConnectProgress();
-//                    List<String> bypassDomains = new LinkedList<>();
-//                    bypassDomains.add("*domain1.com");
-//                    bypassDomains.add("*domain2.com");
-                    final ArrayList<String> domains = new ArrayList<>();
-                    domains.add("ip.me");
-                    UnifiedSdk.getInstance().getVpn().start(new SessionConfig.Builder()
-                            .withReason(TrackingConstants.GprReasons.M_UI)
-                            .withTransportFallback(fallbackOrder)
-                            .withTransport(HydraTransport.TRANSPORT_ID)
-                            .withFireshieldConfig(new FireshieldConfig.Builder()
-                                    .addService(FireshieldConfig.Services.IP)
-                                    .addService(FireshieldConfig.Services.BITDEFENDER)
-                                    .addCategory(FireshieldCategory.Builder.proxy(FireshieldConfig.Categories.SAFE))
-                                    .addCategory(FireshieldCategory.Builder.proxy(FireshieldConfig.Categories.UNSAFE))
-                                    .addCategory(FireshieldCategory.Builder.bypass("safeCategory"))
-                                    .addCategoryRule(FireshieldCategoryRule.Builder.fromDomains("safeCategory", domains))
-                                    .build())
-
-                            .withLocation(selectedCountry)
-//                            .addDnsRule(TrafficRule.dns().bypass().fromDomains(bypassDomains))
-                            .build(), new CompletableCallback() {
+                    UnifiedSdk.getInstance().getVpn().start(getSessionConfig(), new CompletableCallback() {
                         @Override
                         public void complete() {
+                            callback.complete();
                             hideConnectProgress();
                             startUIUpdateTask();
                         }
 
                         @Override
                         public void error(@NonNull VpnException e) {
+                            callback.error(e);
                             hideConnectProgress();
                             updateUI();
 
@@ -167,6 +146,31 @@ public class MainActivity extends UIActivity implements TrafficListener, VpnStat
 
             }
         });
+    }
+
+    private SessionConfig getSessionConfig() {
+        final ArrayList<String> domains = new ArrayList<>();
+        domains.add("ip.me");
+        List<String> fallbackOrder = new ArrayList<>();
+        fallbackOrder.add(HydraTransport.TRANSPORT_ID);
+        fallbackOrder.add(OpenVpnTransport.TRANSPORT_ID_TCP);
+        fallbackOrder.add(OpenVpnTransport.TRANSPORT_ID_UDP);
+        return new SessionConfig.Builder()
+                .withReason(TrackingConstants.GprReasons.M_UI)
+                .withTransportFallback(fallbackOrder)
+                .withTransport(HydraTransport.TRANSPORT_ID)
+                .withFireshieldConfig(new FireshieldConfig.Builder()
+                        .addService(FireshieldConfig.Services.IP)
+                        .addService(FireshieldConfig.Services.BITDEFENDER)
+                        .addCategory(FireshieldCategory.Builder.proxy(FireshieldConfig.Categories.SAFE))
+                        .addCategory(FireshieldCategory.Builder.proxy(FireshieldConfig.Categories.UNSAFE))
+                        .addCategory(FireshieldCategory.Builder.bypass("safeCategory"))
+                        .addCategoryRule(FireshieldCategoryRule.Builder.fromDomains("safeCategory", domains))
+                        .build())
+
+                .withLocation(selectedCountry)
+//                            .addDnsRule(TrafficRule.dns().bypass().fromDomains(bypassDomains))
+                .build();
     }
 
     @Override
@@ -264,36 +268,185 @@ public class MainActivity extends UIActivity implements TrafficListener, VpnStat
         loginToVpn();
     }
 
+    void logRemove(VpnStateListener stateListener) {
+        Log.d(TAG, stateListener + " remove");
+        UnifiedSdk.removeVpnStateListener(stateListener);
+    }
+
+    void logAdd(VpnStateListener stateListener) {
+        Log.d(TAG, stateListener + " add");
+        UnifiedSdk.addVpnStateListener(stateListener);
+    }
+
+    List<VpnStateListener> history = new ArrayList<>();
+
+    class WaitConnectAndReconnect implements VpnStateListener {
+        final int idx;
+
+        WaitConnectAndReconnect(int idx) {
+            this.idx = idx;
+        }
+
+        @Override
+        public String toString() {
+            return "WaitConnectAndReconnect-" + idx;
+        }
+
+        @Override
+        public void vpnStateChanged(@NonNull final VpnState vpnState) {
+            Log.d(TAG, this + " got state " + vpnState);
+            VpnStateListener self = this;
+            if (vpnState == VpnState.CONNECTED) {
+                // get to connected. can restart
+                Log.d(TAG, this + " will call restart");
+                logRemove(self);
+                UnifiedSdk.getInstance().getVpn().restart(getSessionConfig(), new CompletableCallback() {
+                    @Override
+                    public void complete() {
+                        // got connected
+                        Log.d(TAG, self + " got connected");
+
+                    }
+
+                    @Override
+                    public void error(@NonNull VpnException e) {
+                        // got error when restarting. Can try again or show user an error
+                        Log.e(TAG, self + " restart got error", e);
+                        showMessage("Cannot connect to server");
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void vpnError(@NonNull final VpnException e) {
+            // got vpn error from sdk. Can do another connection attempt or report error to user
+            Log.e(TAG, this + " vpnError", e);
+            VpnStateListener self = this;
+            logRemove(self);
+            connectToVpn(new CompletableCallback() {
+                @Override
+                public void complete() {
+                    Log.d(TAG, self + " connect complete");
+                }
+
+                @Override
+                public void error(@NonNull VpnException e) {
+                    Log.e(TAG, self + " connect vpnError", e);
+                }
+            });
+        }
+    }
+
+    ;
+
+    class WaitIdleAndConnect implements VpnStateListener {
+        final int idx;
+
+        WaitIdleAndConnect(int idx) {
+            this.idx = idx;
+        }
+
+        @Override
+        public String toString() {
+            return "WaitIdleAndConnect-" + idx;
+        }
+
+        @Override
+        public void vpnStateChanged(@NonNull final VpnState vpnState) {
+            Log.d(TAG, this + " got state" + vpnState);
+            VpnStateListener self = this;
+            if (vpnState == VpnState.IDLE) {
+                logRemove(this);
+                connectToVpn(new CompletableCallback() {
+                    @Override
+                    public void complete() {
+                        Log.d(TAG, self + " connect complete");
+                    }
+
+                    @Override
+                    public void error(@NonNull VpnException e) {
+                        Log.e(TAG, self + " connect vpnError", e);
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void vpnError(@NonNull final VpnException e) {
+            // got vpn error from sdk. Can do another connection attempt or report error to user
+            VpnStateListener self = this;
+            Log.e(TAG, self + " vpnError", e);
+            logRemove(this);
+            connectToVpn(new CompletableCallback() {
+                @Override
+                public void complete() {
+                    Log.d(TAG, self + " connect complete");
+                }
+
+                @Override
+                public void error(@NonNull VpnException e) {
+                    Log.e(TAG, self + " connect vpnError", e);
+                }
+            });
+        }
+    }
+
+    ;
+    int idx = 0;
+
     @Override
     public void onRegionSelected(RegionListAdapter.Region item) {
 
         selectedCountry = item.getName();
         updateUI();
+        for (VpnStateListener listener : history) {
+            logRemove(listener);
+        }
+
 
         UnifiedSdk.getVpnState(new Callback<VpnState>() {
             @Override
             public void success(@NonNull VpnState state) {
-                if (state == VpnState.CONNECTED) {
-                    showMessage("Reconnecting to VPN with " + selectedCountry);
-                    UnifiedSdk.getInstance().getVpn().stop(TrackingConstants.GprReasons.M_UI, new CompletableCallback() {
-                        @Override
-                        public void complete() {
-                            connectToVpn();
-                        }
+                Log.d(TAG, String.format("onRegionSelected got %s", state));
+                switch (state) {
+                    case IDLE -> {
+                        // when in idle can connect right away
+                        final WaitIdleAndConnect listener = new WaitIdleAndConnect(++idx);
+                        logAdd(listener);
+                        history.add(listener);
+                    }
+                    case CONNECTING_CREDENTIALS, CONNECTING_PERMISSIONS, CONNECTING_VPN -> {
+                        // when in connecting need to wait completion
+                        final WaitConnectAndReconnect listener = new WaitConnectAndReconnect(++idx);
+                        logAdd(listener);
+                        history.add(listener);
+                    }
+                    case PAUSED -> {
+                        // if in paused - need to wait for connected state. Or do something else, depends on UX
+                        final WaitConnectAndReconnect listener = new WaitConnectAndReconnect(++idx);
+                        logAdd(listener);
+                        history.add(listener);
+                    }
+                    case DISCONNECTING -> {
+                        // when in progress of disconnect, wait idle and then connect
+                        final WaitIdleAndConnect listener = new WaitIdleAndConnect(++idx);
+                        logAdd(listener);
+                        history.add(listener);
+                    }
+                    case CONNECTED -> {
+                        // when connected can restart
+                        final WaitConnectAndReconnect listener = new WaitConnectAndReconnect(++idx);
+                        logAdd(listener);
+                        history.add(listener);
+                    }
 
-                        @Override
-                        public void error(VpnException e) {
-                            // In this case we try to reconnect
-                            selectedCountry = "";
-                            connectToVpn();
-                        }
-                    });
                 }
             }
 
             @Override
             public void failure(@NonNull VpnException e) {
-
+                Log.e(TAG, "onRegionSelected got error", e);
             }
         });
     }
