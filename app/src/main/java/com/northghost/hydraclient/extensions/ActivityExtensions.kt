0be826475897733e1df3c64129b19891d18c316e
DEVTECH\andrej.dusnoki <andrej.dusnoki@pango.co>
2025-05-15 13:41:26 +0200

    added try catch

--- app/src/main/java/com/northghost/hydraclient/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/northghost/hydraclient/extensions/ActivityExtensions.kt
@@ -2,15 +2,14 @@ package com.northghost.hydraclient.extensions
 
 import android.app.AlarmManager
 import android.app.AlertDialog
-import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.northghost.hydraclient.R
 import com.scottyab.rootbeer.RootBeer
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -33,30 +32,34 @@ fun AppCompatActivity.scheduleAlarmPermissionGranted() = Build.VERSION.SDK_INT >
             .canScheduleExactAlarms()
 
 fun AppCompatActivity.showDialogIfRooted() {
-    lifecycleScope.launch{
-        val rootBeer = RootBeer(this@showDialogIfRooted)
-        val  rootResult =
-            listOf(
-                Pair("Root Management Apps", rootBeer.detectRootManagementApps()),
-                Pair("Potentially Dangerous Apps", rootBeer.detectPotentiallyDangerousApps()),
-                Pair("Root Cloaking Apps", rootBeer.detectRootCloakingApps()),
-                Pair("TestKeys", rootBeer.detectTestKeys()),
-                Pair("BusyBoxBinary", rootBeer.checkForBusyBoxBinary()),
-                Pair("SU Binary", rootBeer.checkForSuBinary()),
-                Pair("2nd SU Binary check", rootBeer.checkSuExists()),
-                Pair("For RW Paths", rootBeer.checkForRWPaths()),
-                Pair("Dangerous Props", rootBeer.checkForDangerousProps()),
-                Pair("Root via native check", rootBeer.checkForRootNative()),
-                Pair("Magisk specific checks", rootBeer.checkForMagiskBinary()),
-            ).filter { it.second }.joinToString(separator = "\n") { it.first }
-        if (rootResult.isNotEmpty()) {
-            val builder = AlertDialog.Builder(this@showDialogIfRooted)
-            builder.setTitle("Device is rooted").setMessage("Root status triggered via:${rootResult}").setNegativeButton("OK"){ dialog, _ ->
-                dialog.dismiss()
-            }
-            withContext(Dispatchers.Main) {
-                builder.show()
+    try {
+        lifecycleScope.launch{
+            val rootBeer = RootBeer(this@showDialogIfRooted)
+            val  rootResult =
+                listOf(
+                    Pair("Root Management Apps", rootBeer.detectRootManagementApps()),
+                    Pair("Potentially Dangerous Apps", rootBeer.detectPotentiallyDangerousApps()),
+                    Pair("Root Cloaking Apps", rootBeer.detectRootCloakingApps()),
+                    Pair("TestKeys", rootBeer.detectTestKeys()),
+                    Pair("BusyBoxBinary", rootBeer.checkForBusyBoxBinary()),
+                    Pair("SU Binary", rootBeer.checkForSuBinary()),
+                    Pair("2nd SU Binary check", rootBeer.checkSuExists()),
+                    Pair("For RW Paths", rootBeer.checkForRWPaths()),
+                    Pair("Dangerous Props", rootBeer.checkForDangerousProps()),
+                    Pair("Root via native check", rootBeer.checkForRootNative()),
+                    Pair("Magisk specific checks", rootBeer.checkForMagiskBinary()),
+                ).filter { it.second }.joinToString(separator = "\n") { it.first }
+            if (rootResult.isNotEmpty()) {
+                val builder = AlertDialog.Builder(this@showDialogIfRooted)
+                builder.setTitle("Device is rooted").setMessage("Root status triggered via:${rootResult}").setNegativeButton("OK"){ dialog, _ ->
+                    dialog.dismiss()
+                }
+                withContext(Dispatchers.Main) {
+                    builder.show()
+                }
             }
         }
+    }catch (e: Exception) {
+        Log.e("showDialogIfRooted" ,"Exception while detecting root", e)
     }
 }
